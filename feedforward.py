614461a7ea71a0ec284f01ec8491cdc227b60256
Tim Wang <16307130090@fudan.edu.cn>
2021-01-22 02:04:57 +0800

    Add conv layer to the feedforward

--- feedforward.py
+++ feedforward.py
@@ -13,6 +13,12 @@ import numpy as np
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Add a channels dimension
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
 # 打乱数组顺序
 # 指定随机梯度下降的batch大小
 train_ds = tf.data.Dataset.from_tensor_slices(
@@ -23,12 +29,14 @@ test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 class MyModel(Model):
   def __init__(self):
     super(MyModel, self).__init__()
+    self.conv1 = Conv2D(32, 3, activation='relu')
     self.flatten = Flatten()
     self.d1 = Dense(128, activation='relu')
     self.d2 = Dense(32, activation='relu')
     self.d3 = Dense(10, activation='softmax')
 
   def call(self, x):
+    x = self.conv1(x)
     x = self.flatten(x)
     x = self.d1(x)
     x = self.d2(x)
